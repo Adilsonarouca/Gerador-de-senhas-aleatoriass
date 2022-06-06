b1ffe072dbe096f3382a684030ef3d3cf14eafae
Adilsonarouca <adilsonarouca07@gmail.com>
2022-06-06 20:46:17 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijlmnopqrstuvxzkykABCDEFGHIGLMNOPKRSTUVXZKY0123456789!@#*";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function (){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+        novaSenha = pass;
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+}
+
+function copyPassword(){
+    alert("Senha copiada com Sucesso!");
+    navigator.clipboard.writeText(novaSenha);
+}
